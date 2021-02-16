8c9153f9fc187f08d6f001d9468eaaea880955b6
lucyandthelens <lucy@lucyandthelens.com>
2021-02-16 19:59:48 +0000

    Update task-3.2.3.js
    Added a return value

--- module-3/lesson-2/scripts/task-3.2.3.js
+++ module-3/lesson-2/scripts/task-3.2.3.js
@@ -4,11 +4,21 @@ function greet(greeting, name) {
   return greeting + ' ' + name;
 }
 
-var message = greet('Hello', 'Luke');
+var message = greet('Hello', 'Lucy');
     console.log( message ); // Output: Hello Luke
 
 /* EXAMPLE */
 
 // ADD YOUR CODE BELOW HERE
+function FullName( FirstName, LastName)
+ {
+  return( FirstName +'' + LastName); }
+
+  console.log (FullName ('Lucy', 'Allen' ))
+
+  console.log (FullName ('Peter', 'Jones'))
+  
+  console.log (FullName ( 'Neil', 'Armstrong'))
+
 
 // ADD YOUR CODE ABOVE HERE
