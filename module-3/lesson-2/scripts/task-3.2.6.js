0fc7eff0afd21b12e15cf0c4e36af5ce2525d492
lucyandthelens <lucy@lucyandthelens.com>
2021-02-16 20:41:48 +0000

    Update task-3.2.6.js
    Added an and and an or logical operator to the control flow for the temperature variables

---
+++ module-3/lesson-2/scripts/task-3.2.6.js
@@ -0,0 +1,14 @@
+var Temperature = 45;
+
+if(Temperature <0 && ItsIcy ) { 
+    
+    console.log('Stay inside');}
+
+
+else if (Temperature < 50 || Cold ) { 
+    console.log( Temperature + 'Wear A Coat' );}
+
+  else  if (Temperature < 30 && SeasonIsWinter) {console.log("Wear A Coat and a hat")}
+
+  else{ console.log( 'just pants and a vest is fine')}
+
