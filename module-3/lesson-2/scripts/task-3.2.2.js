8f812c6becabf23796aff8e01c8b54bd2f1b7abd
lucyandthelens <lucy@lucyandthelens.com>
2021-02-16 19:40:35 +0000

    Update task-3.2.2.js
    Create: fullname function, added two parameters and concatinated both paramenters together

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,9 @@
+function FullName( FirstName, LastName) {
+    console.log( FirstName +'' + LastName);
+}
+
+FullName ('Lucy', 'Allen' )
+
+FullName ('Peter', 'Jones')
+
+FullName ( 'Neil', 'Armstrong')
