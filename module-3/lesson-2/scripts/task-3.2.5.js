b40a980563f1c3b623bebf594b786a24b37d1b85
lucyandthelens <lucy@lucyandthelens.com>
2021-02-16 20:32:02 +0000

    Update task-3.2.5.js
    add variable for temperature. Added some ifs and else ifs to the control flow.

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,15 @@
+var Temperature = 45;
+
+if(Temperature <0) { 
+    
+    console.log('Stay inside');}
+
+
+else if (Temperature < 50) { 
+    console.log( Temperature + 'Wear A Coat' );}
+
+  else  if (Temperature < 30) {console.log("Wear A Coat and a hat")}
+
+  else{ console.log( 'just pants and a vest is fine')}
+
+  
