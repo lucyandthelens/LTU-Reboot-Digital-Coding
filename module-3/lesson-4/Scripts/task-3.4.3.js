e76088a43bb08f6933c4a2d3d0b39d25a22aa957
lucyandthelens <lucy@lucyandthelens.com>
2021-02-23 20:12:58 +0000

    Create task-3.4.3.js
    Add favourite foods array

---
+++ module-3/lesson-4/Scripts/task-3.4.3.js
@@ -0,0 +1,15 @@
+let favouriteFoods = [
+    'Pasta',
+'Risotto',
+'Oranges', 
+'Burgers',
+ 'Chocolate',
+ 'Chorizo'
+] ;
+
+
+for ( let index = 0; index < favouriteFoods.length; index++ ) {
+
+
+    console.log(favouriteFoods[ index ]);
+}
