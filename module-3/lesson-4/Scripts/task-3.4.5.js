07d47ade5996d1ca2c9528f23dd5c019451d028e
lucyandthelens <lucy@lucyandthelens.com>
2021-02-25 19:13:21 +0000

    Create task-3.4.5.js
    Add Lets Cook Function. Put Function after lets cook

---
+++ module-3/lesson-4/Scripts/task-3.4.5.js
@@ -0,0 +1,26 @@
+const favouriteRecipe = { recipeTitle : 'Spaghetti Bolognese',
+servings: 6,
+ingredients: [
+    '1 tbsp olive oil',
+    ' 500g mince beef',
+    'tomato passata',
+    'onions',
+    'garlic',
+    'mushrooms',
+    'peppers',
+    'basil',
+    'mixed herbs'
+],
+directions: ['Make Bolognese sauce',
+'step 2',
+'step 3'
+]
+},
+
+letsCook: function() {
+    console.log( "I'm hungry, let's cook" + favouriteRecipe.recipeTitle);
+}
+
+favouriteRecipe.letsCook();
+
+
