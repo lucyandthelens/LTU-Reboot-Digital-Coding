d22198c77bfbfff5bdaacaabf3a7b6b844d29962
lucyandthelens <lucy@lucyandthelens.com>
2021-02-25 20:38:08 +0000

    Create task-3.5.3a.js
    added discount amount

---
+++ module-3/lesson-5/scripts/task-3.5.3a.js
@@ -0,0 +1,33 @@
+function discountAmount( arr, discountAmount, type ) {
+
+    let totalPrice = 0;
+
+    for ( let index=0; index < arr.length; index++) {
+
+        if( arr[ index ].type === type) {
+
+            let discount = ( arr[ index].price * discountAmount ) / 100;
+
+            totalPrice = totalPrice + ( arr[ index ].price - discount ) * arr[ index ].quantity;
+
+        
+        } else if { (arr[ index ].type) === 'any' ) {
+
+            let discount = ( arr[ index].price * discountAmount ) / 100 ;
+
+            totalPrice = totalPrice + ( arr[ index ].price - discount ) * arr[ index ].quantity ;
+
+        } else {
+
+            totalPrice = totalPrice + ( arr[ index ] * arr[ index ].quantity ;
+        }
+    
+    }
+
+return totalPrice.toFixed( 2 );
+
+
+}
+
+console.log( discountAmount( shoppingcart , 15 , 'food' ));
+console.log( discountAmount( shoppingcart, 50, 'alchol' ));
