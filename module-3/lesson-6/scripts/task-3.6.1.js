67a759323b239c6d2fc4d6ce63d0fe905b95a78b
lucyandthelens <lucy@lucyandthelens.com>
2021-03-02 19:55:54 +0000

    Create task-3.6.1.js
    did a document query selector and set attributes

---
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -0,0 +1,15 @@
+let paragraphs = document.querySelectorAll('p')
+
+let paragraph3 = paragraphs[2];
+paragraph3.style.border = "4px solid red";
+paragraph3.style.padding = "20px";
+
+
+let logo = document.querySelector('img');
+logo.setAttribute( 'src','https://c.static-nike.com/a/images/w_1920,c_limit/bzl2wmsfh7kgdkufrrjq/image.jpg');
+
+let courseList = document.querySelectorAll('li');
+let courseListItem2 = courseList[1];
+courseListItem2.style.color = "green";
+courseListItem2.style.fontWeight = "bold";
+courseListItem2.style.fontsize = "26px"
